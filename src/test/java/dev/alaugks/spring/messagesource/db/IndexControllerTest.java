1c583471bd240dfd87ef394501165013306d4012
André Laugks <alaugks@gmail.com>
2026-07-15 17:18:06 +0200

    Update Spring Boot version and dependencies
    - Upgrade Spring Boot to version 4.1.0 in pom.xml
    - Replace spring-boot-starter-test with spring-boot-starter-webmvc-test
    - Adjust WebMvcTest import in IndexControllerTest
    - Remove dependency ignore rules from dependabot configuration

--- src/test/java/dev/alaugks/spring/messagesource/db/IndexControllerTest.java
+++ src/test/java/dev/alaugks/spring/messagesource/db/IndexControllerTest.java
@@ -5,7 +5,7 @@ import dev.alaugks.spring.messagesource.db.controller.IndexController;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
